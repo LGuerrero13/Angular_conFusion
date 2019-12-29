a72cb1f03b0b9a69f9f9754f9f06d3dc614241e4
Jose Leonardo Guerrero Herrera <guerreroleonardo33@gmail.com>
2019-12-29 05:36:38 -0500

    Promise Part 1

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -15,7 +15,8 @@ export class AboutComponent implements OnInit {
   constructor(private leaderService: LeaderService) { }
 
   ngOnInit() {
-    this.Leaders = this.leaderService.getLeaders();
+    this.leaderService.getLeaders()
+      .then((Leaders) => this.Leaders = Leaders);
   }
 
 }
